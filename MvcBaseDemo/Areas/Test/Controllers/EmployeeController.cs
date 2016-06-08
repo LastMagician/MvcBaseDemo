fa5953fd30b0e631ff8ef457f2aa27644eae56f6
leng <lzyfems@163.com>
2016-06-08 13:34:29 +0800

    实验9--创建数据入口

---
+++ MvcBaseDemo/Areas/Test/Controllers/EmployeeController.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcBaseDemo.Areas.Test.Models;
+using MvcBaseDemo.Areas.Test.ViewModels;
+
+namespace MvcBaseDemo.Areas.Test.Controllers
+{
+    public class Customer
+    {
+        public string CustomerName { get; set; }
+        public string Address { get; set; }
+
+        public override string ToString()
+        {
+            return this.CustomerName + "|" + this.Address;
+        }
+    }
+
+    public class EmployeeController : Controller
+    {
+        //
+        // GET: /Test/Test/
+
+        public Customer GetCustomer()
+        {
+            Customer c = new Customer();
+            c.CustomerName = "Customer 1";
+            c.Address = "Address 1";
+            return c;
+        }
+
+        // not Action Method defind
+        [NonAction]
+        public string SimpleMethod()
+        {
+            return "Hi, I am not";
+        }
+
+        public string ReturnStr()
+        {
+            return "Ths";
+        }
+
+        public ActionResult Index()
+        {
+            //Employee emp = new Employee();
+            //emp.FirstName = "Sukesh";
+            //emp.LastName = "Marla";
+            //emp.Salary = 20000;
+
+            //EmployeeViewModel vmEmp = new EmployeeViewModel();
+            //vmEmp.EmployeeName = emp.FirstName + "  " + emp.LastName;
+            //vmEmp.Salary = emp.Salary.ToString("C");
+            //if (emp.Salary > 15000)
+            //{
+            //    vmEmp.SalaryColor = "yellow";
+            //}
+            //else
+            //{
+            //    vmEmp.SalaryColor = "green";
+            //}
+            //vmEmp.UserName = "Admin";
+
+            EmployeeListViewModel employeeListViewModel = new EmployeeListViewModel();
+            EmployeeBusinessLayer empBal = new EmployeeBusinessLayer();
+            List<Employee> employees = empBal.GetEmployees();
+            List<EmployeeViewModel> empViewModels = new List<EmployeeViewModel>();
+
+            foreach (Employee emp in employees)
+            {
+                EmployeeViewModel empViewModel = new EmployeeViewModel();
+                empViewModel.EmployeeName = emp.FirstName + " " + emp.LastName;
+                empViewModel.Salary = emp.Salary.ToString("C");
+                if (emp.Salary > 15000)
+                {
+                    empViewModel.SalaryColor = "yellow";
+                }
+                else
+                {
+                    empViewModel.SalaryColor = "green";
+                }
+                empViewModels.Add(empViewModel);
+            }
+
+            employeeListViewModel.Employees = empViewModels;
+
+            //employeeListViewModel.UserName = "Admin";-->Remove this line -->Change1
+            return View("Index", employeeListViewModel);//-->Change View Name -->Change 2
+        }
+
+
+        public ActionResult AddNew()
+        {
+            return View("CreateEmployee");
+        }
+    }
+}
