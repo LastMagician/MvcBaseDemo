dfa33b8418af6b1e4f685596b73dfb32a2cdf4c8
leng <lzyfems@163.com>
2016-06-14 17:03:37 +0800

    1.完成MM[VC文件上传02“文章的作；2.修改.gitignore，上传packages文件夹；

--- MvcBaseDemo/Areas/Test/Controllers/FileUploadController.cs
+++ MvcBaseDemo/Areas/Test/Controllers/FileUploadController.cs
@@ -9,26 +9,149 @@ namespace MvcBaseDemo.Areas.Test.Controllers
 {
     public class FileUploadController : Controller
     {
+        private string DirName = "../../Uploads";
         //
         // GET: /Test/FileUpload/
+
 
         public ActionResult Index()
         {
             return View();
         }
 
+        #region MVC最基本的上传文件是通过form表单提交方式
+        public ActionResult UploadView()
+        {
+            return View("UploadView");
+        }
+
+        //MVC最基本的上传文件是通过form表单提交方式
         [HttpPost]
         public ActionResult BaseMethod(HttpPostedFileBase uploadFile)
         {
-            if (uploadFile.ContentLength > 0)
+            if (uploadFile != null && uploadFile.ContentLength > 0)
             {
-                string filePath = Path.Combine(
-                    HttpContext.Server.MapPath("../Uploads"), 
-                    Path.GetFileName(uploadFile.FileName)
-                );
-                uploadFile.SaveAs(filePath);
+                string pathForSaving = Server.MapPath(DirName);
+                if (this.CreateFolderIfNeeded(pathForSaving))
+                {
+                    //获得保存路径
+                    string filePath = Path.Combine(
+                        HttpContext.Server.MapPath(DirName),
+                        Path.GetFileName(uploadFile.FileName)
+                    );
+                    uploadFile.SaveAs(filePath);
+                    ViewBag.FilePath = HttpContext.Server.MapPath(DirName+"/" + uploadFile.FileName); 
+                }
             }
             return View();
         }
+        #endregion
+
+        #region 使用JQuery文件上传插件实现异步上传
+        //Content Page
+        public ActionResult JUFIndex()
+        {
+            return View();
+        }
+
+        //使用JQuery文件上传插件实现异步上传
+        [HttpPost]
+        public ActionResult JqueryUploadFile()
+        {
+            HttpPostedFileBase myFile = Request.Files["MyFile"];
+            bool isUploaded = false;
+            string message = "上传失败";
+
+            if (myFile != null && myFile.ContentLength != 0)
+            {
+                string pathForSaving = Server.MapPath(DirName);
+                if (this.CreateFolderIfNeeded(pathForSaving))
+                {
+                    try
+                    {
+                        myFile.SaveAs(Path.Combine(pathForSaving, myFile.FileName));
+                        isUploaded = true;
+                        message = "上传成功";
+                    }
+                    catch (Exception ex)
+                    {
+                        message = string.Format("上传文件失败：{0}", ex.Message);
+                    }
+                }
+            }
+            return Json(new { isUploaded = isUploaded, message = message });
+        }
+        #endregion
+
+        #region 上传多个文件
+        //首页Content页
+        public ActionResult MultiUploadIndex()
+        {
+            return View();
+        }
+
+        //上传多个文件
+        public string MultiUpload(IEnumerable<HttpPostedFileBase> files)
+        {
+            string pathForSaving = Server.MapPath(DirName);
+            string result = "上传失败";
+            if (this.CreateFolderIfNeeded(pathForSaving))
+            {
+                foreach (var file in files)
+                {
+                    if (file != null)
+                    {
+                        try
+                        {
+                            var path = Path.Combine(pathForSaving, file.FileName);
+                            file.SaveAs(path);
+                            result = "上传成功";
+
+                        }
+                        catch (Exception)
+                        {
+                            result = "上传失败";
+                        }
+                    }    
+                }
+            }
+            return result;
+            //return RedirectToAction("Index");
+        }
+        #endregion
+
+        #region 检查是否要创建上传文件夹
+        /// <summary>
+        /// 检查是否要创建上传文件夹
+        /// </summary>
+        /// <param name="path">路径</param>
+        /// <returns></returns>
+        private bool CreateFolderIfNeeded(string path)
+        {
+            bool result = true;
+            if (!Directory.Exists(path))
+            {
+                try
+                {
+                    Directory.CreateDirectory(path);
+                }
+                catch (Exception)
+                {
+
+                    //TODO: 处理异常
+                    result = false;
+                }
+            }
+            return result;
+        }
+        #endregion
+        
+        #region test
+        //test
+        public string ReturnPath()
+        {
+            return HttpContext.Server.MapPath("../../Uploads");
+        }
+        #endregion
     }
 }
--- README.md
+++ README.md
@@ -1,7 +1,13 @@
 # MvcBaseDemo
 根据 七天学会ASP.NET MVC 教程一步步做的例子
 
+## MVC文件上传系列
 
 ## Date: 10000.110.1101
 MVC文件上传01-使用jquery异步上传并客户端验证类型和大小
 http://www.cnblogs.com/darrenji/p/3603351.html
+
+## Date: 10000.110.1110
+MVC文件上传02-使用HttpPostedFileBase上传多个文件
+http://www.cnblogs.com/darrenji/p/3614992.html
+
